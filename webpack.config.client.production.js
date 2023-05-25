66c62dee4c1e57982d071488062c01353566dc21
Joseph Yusufov <jey2011@nyu.edu>
2023-05-24 20:44:19 -0400

    working MERN skeleton

---
+++ webpack.config.client.production.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const CURRENT_WORKING_DIR = process.cwd();
+
+const config = {
+  mode: "production",
+  entry: [path.join(CURRENT_WORKING_DIR, "client/main.js")],
+  output: {
+    path: path.join(CURRENT_WORKING_DIR, "/dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+        use: "file-loader",
+      },
+    ],
+  },
+};
+
+module.exports = config;
